812c665338c3c4361141d141d37e1e0c40b6ce9d
siweiwang <wangsiwei2003@hotmail.com>
2016-10-02 14:17:25 -0400

    minification

--- di_app.js
+++ di_app.js
@@ -1,8 +1,13 @@
 (function () {
   'use strict'
 
+  // However, this will have a issue with minification. The name "DIController" will get changed during minification.
+  //angular.module("DIApp", []).controller('DIController', DIController);
+  // To fix that, we need 
   angular.module("DIApp", []).controller('DIController', DIController);
 
+
+  DIController.$inject = ['$scope', '$filter'];
   // $scope and $filter are services here. They are injected by angular framework
   function DIController ($scope, $filter) {
     $scope.name = "Siwei"
